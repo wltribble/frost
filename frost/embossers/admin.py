edfbd31aedf714eccad03554bfeb95844232874d
Will Tribble <wltribble@gmail.com>
2017-06-09 11:02:52 -0400

    Created a process outline model and added it to the admin

--- frost/embossers/admin.py
+++ frost/embossers/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Job, Field
+from .models import Job, Field, Process, OutlineField
 # Register your models here.
 
 class FieldInLine(admin.TabularInline):
@@ -22,6 +22,21 @@ class JobAdmin(admin.ModelAdmin):
     inlines = [FieldInLine]
     list_display = ('job_id', 'date_created', 'was_created_recently')
     list_filter = ['date_created']
+
+
+class OutlineFieldInLine(admin.TabularInline):
+    model = OutlineField
+    extra = 0
+
+class ProcessAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,      {'fields': ['job_id',]}),
+    ]
+    inlines = [OutlineFieldInLine]
+    list_display = ('process_name')
+
+
+
 
 
 admin.site.register(Job, JobAdmin)
--- frost/embossers/models.py
+++ frost/embossers/models.py
@@ -49,3 +49,22 @@ class Field(models.Model):
 
     def __str__(self):
         return self.field_name
+
+
+class Process(models.Model):
+    process_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.process_name
+
+
+class OutlineField(models.Model):
+    process = models.ForeignKey(Process, on_delete=models.CASCADE)
+    OUTLINE_field_name = models.CharField(max_length=100)
+    OUTLINE_field_text = models.CharField(max_length=200, blank=True)
+    OUTLINE_name_is_operator_editable = models.BooleanField(default=True)
+    OUTLINE_text_is_operator_editable = models.BooleanField(default=True)
+    OUTLINE_required_for_full_submission = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.OUTLINE_field_name
