27a4f7773a7a5b9bbd2ba2b50939f57766798493
Will Tribble <wltribble@gmail.com>
2017-06-13 13:34:27 -0400

    Cleaned up migrations to remove an error that was preventing tests

--- frost/embossers/tests.py
+++ frost/embossers/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from django.utils import timezone
 
 from model_mommy import mommy
 
--- frost/frost/settings.py
+++ frost/frost/settings.py
@@ -86,13 +86,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         'TEST': {
-            'DEPENDENCIES': [''],
-        },
-    },
-    'processes': {
-        'TEST': {
-            'DEPENDENCIES': ['default'],
-        },
+            'DEPENDENCIES': [],
+        }
     }
 }
 
