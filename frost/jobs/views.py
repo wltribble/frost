876846b7892abe2bbcec649f6df9ca396b435162
Will Tribble <wltribble@gmail.com>
2017-06-27 10:54:07 -0400

    changed detail view context job declaration

--- frost/jobs/views.py
+++ frost/jobs/views.py
@@ -58,7 +58,8 @@ class DetailView(generic.DetailView):
     def get_context_data(self, **kwargs):
         context = super(DetailView, self).get_context_data(**kwargs)
         for job_iterator in Job.objects.raw('SELECT * FROM JobOperations WHERE [jmouniqueid] = %s', [self.kwargs['urluniqueid']]):
-            context['job'], job = job_iterator
+            context['job'] = job_iterator
+            job = job_iterator
         context['field_set'] = Field.objects.filter(job=job)
         return context
 
