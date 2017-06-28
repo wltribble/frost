bcdb63cf11bb89d11261749c141a5de7b0ca6163
Will Tribble <wltribble@gmail.com>
2017-06-28 14:39:47 -0400

    got rid of staticfiles_dir

--- frost/config/settings/base.py
+++ frost/config/settings/base.py
@@ -113,9 +113,6 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, 'static'))
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
 
 try:
     from .dev import *
