73741c15f5252eca8eb6d2c07935e39d62d3eb00
Will Tribble <wltribble@gmail.com>
2017-06-29 16:39:10 -0400

    switched up workcenter urls

--- frost/workcenters/urls.py
+++ frost/workcenters/urls.py
@@ -5,6 +5,6 @@ from .views import PickCenterView
 
 app_name = 'workcenters'
 urlpatterns = [
+    url(r'^(?P<center_pk>[0-9]+)/jobs/', include('jobs.urls', namespace='jobs')),
     url(r'^$', PickCenterView.as_view(), name='pick_center'),
-    url(r'^(?P<center_pk>[0-9]+)/jobs/$', include('jobs.urls', namespace='jobs')),
 ]
