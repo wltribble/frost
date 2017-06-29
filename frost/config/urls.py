ac89266178413dae174cd414e8ea11a30cdb203d
Will Tribble <wltribble@gmail.com>
2017-06-29 16:24:54 -0400

    added workcenters to config.urls

--- frost/config/urls.py
+++ frost/config/urls.py
@@ -3,5 +3,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^jobs/', include('jobs.urls', namespace='jobs')),
+    url(r'^$', include('workcenters.urls', namespace='workcenters')),
 ]
