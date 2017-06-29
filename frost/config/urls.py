6c9168554e82fe1795530283559fc308e0891f29
Will Tribble <wltribble@gmail.com>
2017-06-29 16:54:26 -0400

    serious url namespace rework

--- frost/config/urls.py
+++ frost/config/urls.py
@@ -3,5 +3,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^(?P<center_pk>[0-9]+)/jobs/', include('jobs.urls', namespace='jobs')),
     url(r'^', include('workcenters.urls', namespace='workcenters')),
 ]
