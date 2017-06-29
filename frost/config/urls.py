19bd8e9725692ff4f0f6f7f635523ee268559840
Will Tribble <wltribble@gmail.com>
2017-06-29 16:51:17 -0400

    no more $ in blank url regex

--- frost/config/urls.py
+++ frost/config/urls.py
@@ -3,5 +3,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', include('workcenters.urls', namespace='workcenters')),
+    url(r'^', include('workcenters.urls', namespace='workcenters')),
 ]
