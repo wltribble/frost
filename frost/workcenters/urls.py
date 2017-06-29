b7e33ba5b0d026e845af57fd682495d5156a993a
Will Tribble <wltribble@gmail.com>
2017-06-29 16:33:06 -0400

    fixed regex issue workcenter urls

--- frost/workcenters/urls.py
+++ frost/workcenters/urls.py
@@ -6,5 +6,5 @@ from .views import PickCenterView
 app_name = 'workcenters'
 urlpatterns = [
     url(r'^$', PickCenterView.as_view(), name='pick_center'),
-    url(r'^(?P<center_pk>[0-9]+/jobs/$', include('jobs.urls', namespace='jobs')),
+    url(r'^(?P<center_pk>[0-9]+)/jobs/$', include('jobs.urls', namespace='jobs')),
 ]
