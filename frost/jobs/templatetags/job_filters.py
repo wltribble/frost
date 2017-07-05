17b4893535b3c6e46bc76674922fb002b0de6b6b
Will Tribble <wltribble@gmail.com>
2017-07-05 13:08:09 -0400

    restrict job filter to show only by work center

--- frost/jobs/templatetags/job_filters.py
+++ frost/jobs/templatetags/job_filters.py
@@ -6,7 +6,7 @@ register = template.Library()
 
 @register.inclusion_tag('jobs/misc/fields_by_center.html')
 def fields_by_center(job, center):
-    jobs = Job.objects.all().filter(jmojobid=job)
+    jobs = Job.objects.all().filter(jmojobid=job).filter(jmoworkcenterid=center)
 
     counter = 0
     fields_needed = []
