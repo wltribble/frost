bbd82a91089bd69f7f5c1517bf0c6348bfc2cc81
Will Tribble <wltribble@gmail.com>
2017-07-10 17:49:59 -0400

    fixed url issue

--- frost/jobs/urls.py
+++ frost/jobs/urls.py
@@ -21,5 +21,5 @@ urlpatterns = [
     url(r'^manager/$', ManagerIndex.as_view(), name='manager_index'),
     url(r'^manager/(?P<jobid>[0-9a-zA-Z-]+)/$', ManagerDataView.as_view(), name='manager_data_view'),
     url(r'^manager/(?P<jobid>[0-9a-zA-Z-]+)/create_report/$', ManagerCreateReport.as_view(), name='create_report'),
-    url(url(r'^manager/(?P<jobid>[0-9a-zA-Z-]+)/populate/$', populate, name="populate_report"),
+    url(r'^manager/(?P<jobid>[0-9a-zA-Z-]+)/populate/$', populate, name="populate_report"),
 ]
