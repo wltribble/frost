930a87da6f404f69454bde6bc4ee664a6e754cdc
Will Tribble <wltribble@gmail.com>
2017-07-10 18:07:04 -0400

    add report as a context var for job filters

--- frost/jobs/templatetags/job_filters.py
+++ frost/jobs/templatetags/job_filters.py
@@ -21,12 +21,12 @@ def fields_by_center(job, center):
     return {'fields_needed': fields_needed, 'center': center}
 
 @register.inclusion_tag('jobs/misc/operations_by_job.html')
-def operations_by_job(job):
+def operations_by_job(job, report):
     jobs = Job.objects.all().filter(jmojobid=job)
-    return {'operations': jobs}
+    return {'operations': jobs, 'report': report}
 
 @register.inclusion_tag('jobs/misc/fields_by_operation.html')
-def fields_by_operation(job, assembly, operation):
+def fields_by_operation(job, assembly, operation, report):
     jobs = Job.objects.all().filter(jmojobid=job).filter(jmojobassemblyid=assembly).filter(jmojoboperationid=operation)
 
     counter = 0
@@ -39,4 +39,4 @@ def fields_by_operation(job, assembly, operation):
                 pass
         for field in Field.objects.all().filter(job=operation.jmouniqueid).filter(is_a_meta_field=False).filter(submission_number=counter):
             fields_needed.append(field)
-    return {'fields_needed': fields_needed}
+    return {'fields_needed': fields_needed, 'report': report}
