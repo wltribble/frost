8c482384f199c40c2de68bd530a60fb0b2f0567d
Will Tribble <wltribble@gmail.com>
2017-07-12 13:58:22 -0400

    added reopen of 0 to engineering

--- frost/workcenters/views.py
+++ frost/workcenters/views.py
@@ -95,6 +95,7 @@ class EngineeringDetailView(generic.DetailView):
 
 
         context['center'] = job.jmoworkcenterid
+        context['reopen_number'] = 0
         context['job_instructions'] = (
                                     JobInstructions.objects.all(
                                     ).filter(
