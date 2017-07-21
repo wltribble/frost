ee92a786fc2c30549530e14cc13caa229871f8d9
Will Tribble <wltribble@gmail.com>
2017-07-21 12:11:17 -0400

    moved employee urls to app processes

--- frost/config/urls.py
+++ frost/config/urls.py
@@ -4,6 +4,6 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^center/', include('jobs.urls', namespace='jobs')),
-    url(r'^employee/', include('jobs.employee_urls', namespace='employees')),
+    url(r'^employee/', include('processes.urls', namespace='employees')),
     url(r'^', include('workcenters.urls', namespace='workcenters')),
 ]
--- frost/processes/urls.py
+++ frost/processes/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import url
 
-from .views import EmployeeIndexView
+from jobs.views import EmployeeIndexView
 
-app_name = 'jobs'
+app_name = 'processes'
 urlpatterns = [
     url(r'^(?P<employee_id>[0-9a-zA-Z-]+)/$', EmployeeIndexView.as_view(), name='employee_index'),
 ]
