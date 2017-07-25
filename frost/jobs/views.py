2a1421278699bb4e4438a00ecc4348e8adfb31f0
Will Tribble <wltribble@gmail.com>
2017-07-25 10:00:25 -0400

    exclude active =0 for proj man page

--- frost/jobs/views.py
+++ frost/jobs/views.py
@@ -440,15 +440,15 @@ class ManagerIndex(generic.ListView):
 
     def get_queryset(self):
         search_query = self.request.GET.get('search_box', 'xxxxxxxxxxxxx')
-        return Job.objects.filter(jmojobid__icontains=search_query)[:20]
+        return Job.objects.filter(jmojobid__icontains=search_query).exclude(active=0)
 
     def get_context_data(self, **kwargs):
         context = super(ManagerIndex, self).get_context_data(**kwargs)
         search_query = self.request.GET.get('search_box', 'xxxxxxxxxxxxxxx')
         jobs = (Job.objects.all().filter(
                                     jmojobid__icontains=search_query
-                                    ).order_by('-jmocreateddate')
-                                    )[:20]
+                                    ).order_by('-jmocreateddate').exclude(active=0)
+                                    )
         unique_job_ids = []
         for job in jobs:
             if job.jmojobid != '':
